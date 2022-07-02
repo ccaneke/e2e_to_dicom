6a2e0a0074468dd17aa3142dccb34930567946f4
Chukwuemezie Christopher Aneke <chukwuemezie.aneke.15@ucl.ac.uk>
2022-07-02 18:28:47 +0200

    Add license to source files

--- E2EFileInterpreter/Header.cs
+++ E2EFileInterpreter/Header.cs
@@ -1,4 +1,10 @@
-﻿using System;
+/* Copyright (C) Interneuron, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Chukwuemezie Aneke <ccanekedev@gmail.com>, May 2020
+ */
+
+using System;
 namespace E2EFileInterpreter
 {
     public struct Header
--- E2EFileInterpreter/Program.cs
+++ E2EFileInterpreter/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+/* Copyright (C) Interneuron, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Chukwuemezie Aneke <ccanekedev@gmail.com>, May 2020
+ */
+
+using System;
 using System.Threading.Tasks;
 using System.IO;
 using System.Collections.Generic;
